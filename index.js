1809bc1cc8db1c3ef09f63db02475c3fa25b704a
Argragas <avezard.jeason@gmail.com>
2020-05-02 15:20:57 +0200

    add discord bot

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 var JSSoup = require('jssoup').default;
+
 const SlackBot = require('slackbots');
+const token = require('./token');
+
+const Discord = require('discord.js')
+
+
 const axios = require('axios');
 const fs = require('fs');
 
-const token = require('./token');
+// apps connection
 
+// DISCORD
+const botDiscord = new Discord.Client();
+botDiscord.login(token.tokenDiscord);
+
+botDiscord.on('ready', function () {
+  console.log('discord connection ok ')
+});
+
+// SLACK
+
+const botSlack = new SlackBot({
+  token: token.tokenSlack,
+  name: 'CommitStripBot'
+});
+
+botSlack.on('start', () => {
+  console.log('slack connection ok ')
+});
+
+botSlack.on('error', err => console.log(err));
+
+// SCRAP
 var LastStrip = '';
 
 const getHTML =  () => {
@@ -18,47 +46,45 @@ const getHTML =  () => {
 const getLastStrip =  () => {
   return  new Promise((resolve, reject) => { 
     getHTML().then( response => {
-    var soup = new JSSoup(response.data);
-    var tag = soup.findAll('div','excerpt')
-    const url = tag[0].nextElement.nextElement.nextElement.attrs.href
-    resolve(url);
-  });
+      var soup = new JSSoup(response.data);
+      var tag = soup.findAll('div','excerpt')
+      const url = tag[0].nextElement.nextElement.nextElement.attrs.href
+      resolve(url);
+    });
   });
 
 }
 
 function isNewStrip(){
   console.log('tic')
+  readFile()
   getLastStrip().then( value => {
     if (value !== LastStrip) {
       LastStrip = value;
       writeFile(LastStrip);
       console.log(LastStrip)
-      bot.postMessageToChannel(
+      // slack msg
+      botSlack.postMessageToChannel(
         'troll',
         value
-      ); 
+      );
+      // discord msg
+      msgToDiscord(value);
     }
   })
 }
   
-const bot = new SlackBot({
-  token: token.token,
-  name: 'CommitStripBot'
-});
 
-// Start
-bot.on('start', () => {
-  readFile()
-  isNewStrip();
-  var i = setInterval(function(){
-    isNewStrip();
-}, 3600000);
-});
 
-// Error
-bot.on('error', err => console.log(err));
 
+// DISCORD
+
+function msgToDiscord(url) {
+  const trollChannel = botDiscord.channels.cache.find(channel => channel.name === 'troll');
+  trollChannel.send(url);
+}
+
+// UTILS
 
 function writeFile(text) {
   fs.writeFile('lastCommit', text, 'utf8',function(err) {
@@ -75,3 +101,10 @@ function readFile() {
       LastStrip = data;
   }});
 }
+
+// main 
+
+isNewStrip();
+var i = setInterval(function(){
+  isNewStrip();
+}, 3600000);
